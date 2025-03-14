13729e660d8b395f458cecbb516719c904530e24
Max Bolotin <maks.bolotin@gmail.com>
2025-03-14 21:10:40 +0100

    ok this feels pretty good i think

--- keyboards/planck/rev7/keymaps/maxplanck/keymap.c
+++ keyboards/planck/rev7/keymaps/maxplanck/keymap.c
@@ -63,14 +63,14 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     }
 }
 
-// bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
-//     switch (keycode) {
-//         case MT_LSFT_SPC:
-//             return false;
-//         default:
-//             return true;
-//     }
-// }
+bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case MT_LSFT_SPC:
+            return false;
+        default:
+            return true;
+    }
+}
 
 const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM = LAYOUT_planck_grid(
     'L',  'L',    'L',    'L',    'L',    'L',    'R',    'R',    'R',    'R',    'R', 'R',
